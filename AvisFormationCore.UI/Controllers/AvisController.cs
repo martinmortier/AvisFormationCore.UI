ff3ee1d123d65034a799fc0bba3d333e288ffdbf
Martin Mortier <martin.mortier@gmail.com>
2020-08-18 23:04:21 +0200

    formation part 1

---
+++ AvisFormationCore.UI/Controllers/AvisController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvisFormationCore.UI.Controllers
+{
+    public class AvisController : Controller
+    {
+        public IActionResult LaisserUnAvis(string idFormation)
+        {
+            int id = -1;
+            int.TryParse(idFormation,out id);
+            FormationMemoryRepository repository = new FormationMemoryRepository();
+            Formation formation = repository.getFormationById(id);
+            if(formation == null)
+            {
+                return RedirectToAction("ToutesLesFormations","Formation");
+            }
+            return View(formation);
+        }
+        [HttpPost]
+        public IActionResult SaveComment(string nom, string commentaire,string note)
+        {
+            return View();
+        }
+    }
+}
