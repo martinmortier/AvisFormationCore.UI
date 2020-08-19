41bf3d4bd9f9e557d1ffd535a70fe242c17cd419
Martin Mortier <martin.mortier@gmail.com>
2020-08-19 16:51:56 +0200

    Exo contact

---
+++ AvisFormationCore.UI/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AvisFormationCore.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvisFormationCore.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new EnvoiEmailViewModel();
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult SaveMessage(EnvoiEmailViewModel envoiEmailViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", envoiEmailViewModel);
+            }
+            return View();
+        }
+    }
+}
---
+++ AvisFormationCore.UI/Models/EnvoiEmailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvisFormationCore.UI.Models
+{
+    public class EnvoiEmailViewModel
+    {
+        public string Nom { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [StringLength(20)]
+        public string Message { get; set; }
+    }
+}
